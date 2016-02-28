e697418e5a9d7d3d5723b7d6b9b83024d8d0f69f
Simon Kurka <dev@simon.kurka.cc>
2016-02-28 17:02:36 +0100

    making offset argument optional

--- lib/puppet/parser/functions/ip_broadcast.rb
+++ lib/puppet/parser/functions/ip_broadcast.rb
@@ -3,7 +3,8 @@ require 'netaddr'
 module Puppet::Parser::Functions
   newfunction(:ip_broadcast, :type => :rvalue) do |args|
     ip = NetAddr::CIDR.create(args[0])
-    raise ArgumentError, 'offset out of bounds' if args[1] < 0 || args[1] >= ip.size
-    ip[ip.size-1-((args[1] || '0').to_i)].resize(ip.bits).to_s
+    offset = ((args[1] || '0').to_i)
+    raise ArgumentError, 'offset out of bounds' if offset < 0 || offset >= ip.size
+    ip[ip.size-1-offset].resize(ip.bits).to_s
   end
 end
--- lib/puppet/parser/functions/ip_network.rb
+++ lib/puppet/parser/functions/ip_network.rb
@@ -3,7 +3,8 @@ require 'netaddr'
 module Puppet::Parser::Functions
   newfunction(:ip_network, :type => :rvalue) do |args|
     ip = NetAddr::CIDR.create(args[0])
-    raise ArgumentError, 'offset out of bounds' if args[1] < 0 || args[1] >= ip.size
-    ip[((args[1] || '0').to_i)].resize(ip.bits).to_s
+    offset = ((args[1] || '0').to_i)
+    raise ArgumentError, 'offset out of bounds' if offset < 0 || offset >= ip.size
+    ip[offset].resize(ip.bits).to_s
   end
 end
