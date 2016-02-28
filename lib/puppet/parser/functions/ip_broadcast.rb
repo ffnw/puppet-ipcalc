c5b6c0257777f9569d0e2a1019cc2db8f2e1fdf7
Simon Kurka <dev@simon.kurka.cc>
2016-02-28 14:38:00 +0100

    return values were wrong

--- lib/puppet/parser/functions/ip_broadcast.rb
+++ lib/puppet/parser/functions/ip_broadcast.rb
@@ -4,6 +4,6 @@ module Puppet::Parser::Functions
   newfunction(:ip_broadcast, :type => :rvalue) do |args|
     ip = NetAddr::CIDR.create(args[0])
     raise ArgumentError, 'offset out of bounds' if args[1] < 0 || args[1] >= ip.size
-    ip[ip.size-1-((args[1] || '0').to_i)]
+    ip[ip.size-1-((args[1] || '0').to_i)].resize(ip.bits).to_s
   end
 end
--- lib/puppet/parser/functions/ip_network.rb
+++ lib/puppet/parser/functions/ip_network.rb
@@ -4,6 +4,6 @@ module Puppet::Parser::Functions
   newfunction(:ip_network, :type => :rvalue) do |args|
     ip = NetAddr::CIDR.create(args[0])
     raise ArgumentError, 'offset out of bounds' if args[1] < 0 || args[1] >= ip.size
-    ip[((args[1] || '0').to_i)]
+    ip[((args[1] || '0').to_i)].resize(ip.bits).to_s
   end
 end
